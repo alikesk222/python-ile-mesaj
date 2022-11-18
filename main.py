25ba6e4393b45ef8e4efdf6a1f181e248ef58718
alikesk222 <92251104+alikesk222@users.noreply.github.com>
2022-11-18 20:14:51 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+öncelikle terminalden kütüphaneyi indirip aktifleştirmemiz gerek
+
+pip install pywhatkit
+
+"""
+
+import pywhatkit
+
+pywhatkit.sendwhatmsg('telefon numarası', 'gönderilecek metin', 22,56)
+
+#telefon numarası metin ve gönderilecek saati yazıp çalıştır.
